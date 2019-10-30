e15671d59611747e8dc6580b283559d5469b5bac
Emery <emery.weist.17@cnu.edu>
2019-10-29 23:25:43 -0400

    Some setup

--- StringParserClass.cpp
+++ StringParserClass.cpp
@@ -7,6 +7,58 @@
 
 #include <string>
 #include <string.h>
+#include "../327_proj3_test/includes/StringParserClass.h"
+#include "../327_proj3_test/includes/constants.h"
 
-//TODO Fill in
+using namespace KP_StringParserClass;
+
+char *pStartTag;
+char *pEndTag;
+bool areTagsSet;
+
+StringParserClass::StringParserClass(void)
+{
+	pStartTag = NULL;
+	pEndTag = NULL;
+	areTagsSet = false;
+}
+
+StringParserClass::~StringParserClass(void)
+{
+
+}
+
+int StringParserClass::setTags(const char *pStart, const char *pEnd)
+{
+	if (pStart == NULL || pEnd == NULL)
+		return ERROR_TAGS_NULL;
+
+	return SUCCESS;
+}
+
+int StringParserClass::getDataBetweenTags(char *pDataToSearchThru,
+		std::vector<std::string> &myVector)
+{
+	if (pDataToSearchThru == NULL)
+		return ERROR_DATA_NULL;
+
+	myVector.clear();
+
+	return SUCCESS;
+}
+
+void StringParserClass::cleanup()
+{
+
+}
+
+int StringParserClass::findTag(char *pTagToLookFor, char *&pStart, char *&pEnd)
+{
+	if (pStart == NULL || pEnd == NULL)
+		return ERROR_TAGS_NULL;
+
+	return SUCCESS;
+}
+
+
 
